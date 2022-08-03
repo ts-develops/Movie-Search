a91dce02c63a0851bc398d69dd1db4f8cd3f4f78
ts-develops <tawfiqrahman00@gmail.com>
2022-08-03 19:00:31 +0100

    movie card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {useEffect} from "react";
+import MovieCard from "./MovieCard";
+import './App.css'
+import SearchIcon from './search.svg'
 
-// 6f262a79
 const API = 'http://www.omdbapi.com?apikey=6f262a79'
 
 const App = () => {
@@ -12,12 +14,35 @@ const App = () => {
         console.log(data.Search)
     }
 
+    const movie1 = {
+        "Title": "Italian Spiderman",
+        "Year": "2007",
+        "imdbID": "tt2705436",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+    }
+
     useEffect(() => {
         searchMovies('Spiderman')
     }, []);
 
     return( 
-        <h1>Hello</h1>
+        <div className="app">
+            <h1> MovieSearch</h1>
+
+            <div className="search">
+                <input 
+                placeholder="Search your Movie"
+                value=''
+                onChange={() => {}}
+                />
+                <img src={SearchIcon} alt='search' onClick={() => {}}/>
+            </div>
+
+            <div className="container">
+            <MovieCard movie1={movie1}/>
+            </div>
+        </div>
     );
 }
 
